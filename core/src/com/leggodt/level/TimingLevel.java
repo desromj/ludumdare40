1890eb64e3abb55b7fa1d78dd9921d7d9dba1194
Arne S Jegers <arnesjegers@gmail.com>
2017-12-02 10:45:18 +0100

    Created TimingLevel for the spacebar timing game.

---
+++ core/src/com/leggodt/level/TimingLevel.java
@@ -0,0 +1,22 @@
+package com.leggodt.level;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class TimingLevel extends Level {
+
+    public TimingLevel(OrthographicCamera c){
+        super(c);
+    }
+
+    public void render(float delta){
+        super.render(delta);
+    }
+
+    public void initializeActors(){
+
+    }
+
+    public void handleLoss(){
+
+    }
+}
