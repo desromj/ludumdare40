1eff1daa540d52e7364e649c0ca151820aebd4c6
Arne S Jegers <arnesjegers@gmail.com>
2017-12-02 16:02:39 +0100

    Added class skeletons for Sequence minigame

---
+++ core/src/com/leggodt/level/SequenceButton.java
@@ -0,0 +1,6 @@
+package com.leggodt.level;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class SequenceButton extends Actor {
+}
---
+++ core/src/com/leggodt/level/SequenceLevel.java
@@ -0,0 +1,9 @@
+package com.leggodt.level;
+
+import com.leggodt.util.Clock;
+
+public class SequenceLevel {
+    Clock clock;
+
+
+}
