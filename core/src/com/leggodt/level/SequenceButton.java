62e149d9f33d830ed55f5916a7aca156447918ab
Arne S Jegers <arnesjegers@gmail.com>
2017-12-02 23:48:41 +0100

    Added timer bar to SequenceButton

--- core/src/com/leggodt/level/SequenceButton.java
+++ core/src/com/leggodt/level/SequenceButton.java
@@ -6,8 +6,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.leggodt.util.Clock;
 import com.leggodt.util.Constants;
+import com.leggodt.util.ProgressBar;
 
 public class SequenceButton extends Actor {
+    ProgressBar timerBar;
     static final float scale = 4;
     static float getSpriteWidth(){
         return Constants.spriteC.getWidth()*scale;
@@ -30,12 +32,17 @@ public class SequenceButton extends Actor {
             sprite = Constants.spriteX;
         }
 
+        timerBar = new ProgressBar(0, 0, (int) getSpriteWidth(), (int) (getSpriteWidth()*0.1f));
+
+        timerBar.setForegroundColor(1, 0.73f, 0.82f);
+        timerBar.setBackgroundColor(0.37f, 0.04f, 0.15f);
         clock = new Clock(true);
     }
 
     public void act(float delta) {
         clock.tick();
         isActive = Constants.SEQUENCE_BUTTON_TIME - clock.getTimeSeconds() > 0;
+        timerBar.update((Constants.SEQUENCE_BUTTON_TIME - clock.getTimeSeconds())/Constants.SEQUENCE_BUTTON_TIME);
     }
 
     public void draw(Batch b, float parentAlpha) {
@@ -46,6 +53,12 @@ public class SequenceButton extends Actor {
                 sprite.getWidth() * scale,
                 sprite.getHeight() * scale
         );
+        timerBar.draw(b, parentAlpha);
+    }
+
+    public void setPosition(float x, float y){
+        super.setPosition(x, y);
+        timerBar.setPosition(x + getSpriteWidth()/2 - timerBar.getWidth()/2, getY()+getSpriteWidth()+10);
     }
 
     public int getKey() {
