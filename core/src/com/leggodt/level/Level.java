225e4da6f5c9846bbb67f3ba538cdf063a7d0cc6
Arne S Jegers <arnesjegers@gmail.com>
2017-12-02 13:58:46 +0100

    Added Level-specific background colors

--- core/src/com/leggodt/level/Level.java
+++ core/src/com/leggodt/level/Level.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.leggodt.util.Constants;
 
 public abstract class Level {
+    private float[] bgc = new float[4]; //background color
 
     Stage stage;
     boolean active;
@@ -16,12 +17,14 @@ public abstract class Level {
     public Level(OrthographicCamera camera) {
         stage = new Stage(new StretchViewport(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, camera));
         active = false;
+        setBackgroundColor(0, 0, 0);
     }
 
-
     public abstract void initializeActors();
+
     public abstract void handleLoss();
 
+    public abstract void handleSuccess();
 
     public void render(float delta) {
         if (!active) {
@@ -30,7 +33,7 @@ public abstract class Level {
 
         stage.act(delta);
         stage.getViewport().apply();
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(bgc[0], bgc[1], bgc[2], 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.draw();
     }
@@ -39,13 +42,26 @@ public abstract class Level {
         this.active = active;
     }
 
-    public boolean isActive() { return this.active; }
+    public boolean isActive() {
+        return this.active;
+    }
 
-    public void move(float x, float y){
+    public void move(float x, float y) {
         stage.getViewport().setScreenPosition(MathUtils.round(x), MathUtils.round(y));
     }
 
-    public void resize(float w, float h){
+    public void resize(float w, float h) {
         stage.getViewport().setScreenSize(MathUtils.round(w), MathUtils.round(h));
+    }
+
+    public void setBackgroundColor(float r, float g, float b, float a) {
+        bgc[0] = r;
+        bgc[1] = g;
+        bgc[2] = b;
+        bgc[3] = a;
+    }
+
+    public void setBackgroundColor(float r, float g, float b) {
+        setBackgroundColor(r, g, b, 1);
     }
 }
