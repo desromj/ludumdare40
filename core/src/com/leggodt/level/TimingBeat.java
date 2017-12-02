c04577ac5fbe22b6b2858f9b2fae9c0d8aea18e9
Arne S Jegers <arnesjegers@gmail.com>
2017-12-02 11:29:27 +0100

    Moved TimingLevel logic to TimingBeat. Added beat list to TimingLevel.

--- core/src/com/leggodt/level/TimingBeat.java
+++ core/src/com/leggodt/level/TimingBeat.java
@@ -1,22 +1,59 @@
 package com.leggodt.level;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.leggodt.util.Constants;
 
 public class TimingBeat extends Actor {
     float targetValue;
     float currentValue;
     float margin;
 
-    public TimingBeat() {
+    boolean isActive;
+    boolean isCorrect;
+    boolean hasBeenHit;
+
+    TimingLevel level;
+
+    public TimingBeat(TimingLevel level) {
         super();
+        targetValue = Constants.BEAT_TIME;
+        this.level = level;
     }
 
     public void act(float delta){
         currentValue += delta;
+        isCorrect = MathUtils.isZero(currentValue - targetValue, margin);
+        isActive = currentValue < targetValue + margin;
 
+        //Too late
+        if(!isActive && !hasBeenHit){
+            level.handleLoss();
+            return;
+        }
+
+        boolean keyPressed = Gdx.input.isKeyPressed(Input.Keys.SPACE);
+
+        //only allow 1 button press
+        if(keyPressed && !hasBeenHit){
+            hasBeenHit = true;
+
+            //Deduct points if not within margin
+            if(!isCorrect){
+                level.handleLoss();
+            } else {
+                level.handleSuccess();
+            }
+        }
     }
 
     public void draw(){
 
+    }
+
+    public boolean getActive(){
+        return isActive;
     }
 }
--- core/src/com/leggodt/level/TimingLevel.java
+++ core/src/com/leggodt/level/TimingLevel.java
@@ -2,14 +2,28 @@ package com.leggodt.level;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimingLevel extends Level {
+
+    List<TimingBeat> beats;
 
     public TimingLevel(OrthographicCamera c){
         super(c);
+        beats = new ArrayList<TimingBeat>();
     }
 
     public void render(float delta){
         super.render(delta);
+
+        //Remove beats that have passed
+        for(int i = beats.size()-1; i >= 0; i--){
+            TimingBeat b = beats.get(i);
+            if(!b.getActive()){
+                beats.remove(b);
+            }
+        }
     }
 
     public void initializeActors(){
@@ -17,6 +31,14 @@ public class TimingLevel extends Level {
     }
 
     public void handleLoss(){
+
+    }
+
+    public void handleSuccess(){
+
+    }
+
+    void createBeat(){
 
     }
 }
