8556cc9ac4d551e8e7d2fa3c35a55646ab6366e4
Arne S Jegers <arnesjegers@gmail.com>
2017-12-03 13:59:54 +0100

    Added music to the game.

--- core/src/com/leggodt/DareGame.java
+++ core/src/com/leggodt/DareGame.java
@@ -3,6 +3,7 @@ package com.leggodt;
 import com.badlogic.gdx.Game;
 import com.leggodt.screen.GameScreen;
 import com.leggodt.screen.StartScreen;
+import com.leggodt.util.MusicController;
 
 public class DareGame extends Game {
 
@@ -18,9 +19,12 @@ public class DareGame extends Game {
 
     public static void setScreen(Class type) {
         if (type == GameScreen.class) {
+            MusicController.play(MusicController.GAME);
             instance.setScreen(GameScreen.getInstance());
             GameScreen.getInstance().init();
+
         } else if (type == StartScreen.class) {
+            MusicController.play(MusicController.MENU);
             instance.setScreen(new StartScreen(
                     "LGG_logo.png",
                     "Lava Lamp Bae",
